9387165a93c637950fff2666a82fb976c6d7d7bd
Arsallan Shahab <mdarsallan@gmail.com>
2024-05-10 02:19:37 +0530

    chore: Update npm dependencies and add lucide-react for icon support and update metadata

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,9 +3,18 @@
 import { THEME_STORAGE_KEY } from "@/lib/consts";
 import { Button, Divider, Switch } from "@nextui-org/react";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { LayoutGrid } from "lucide-react";
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
 import { useGlobalContext } from "./context";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
 const Navbar = () => {
   const { theme, setTheme } = useGlobalContext();
   const [isSelected, setIsSelected] = useState<boolean>(false);
@@ -48,23 +57,50 @@ const Navbar = () => {
       >
         <Link
           href="/"
-          className="flex items-center justify-start gap-2.5 font-bricolage text-xl font-semibold dark:text-zinc-100"
+          className="flex items-center justify-start gap-2.5 font-bricolage text-base font-semibold dark:text-zinc-100"
         >
-          DK{" "}
-          <Divider
+          Dhikr / Tasbeeh Counter
+          {/* <Divider
             orientation="vertical"
             className="h-4 w-[2px] bg-black dark:bg-zinc-300"
           />{" "}
-          <span className="text-sm font-medium">Dhikr Counter</span>
+          <span className="text-sm font-medium">Dhikr Counter</span> */}
         </Link>
         <div className="flex items-center space-x-3">
-          <Switch
-            isSelected={isSelected}
-            onValueChange={setIsSelected}
-            color="success"
-            startContent={<SunIcon />}
-            endContent={<MoonIcon />}
-          />
+          <Sheet>
+            <SheetTrigger>
+              <LayoutGrid className="h-5 w-5" />
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader className="justify-start text-left">
+                <SheetTitle>Settings</SheetTitle>
+                <SheetDescription>
+                  <div className="flex items-center justify-between rounded-xl bg-zinc-100 p-2 dark:bg-zinc-900">
+                    <span>Dark Mode</span>
+                    <Switch
+                      isSelected={isSelected}
+                      onValueChange={setIsSelected}
+                      color="success"
+                      startContent={<SunIcon />}
+                      endContent={<MoonIcon />}
+                    />
+                  </div>
+
+                  <Divider className="my-3" />
+
+                  <p className="text-xs font-medium dark:text-zinc-100">
+                    Any feedback or suggestions?{" "}
+                    <a
+                      href="mailto:mdarsallan@gmail.com"
+                      className="text-blue-500"
+                    >
+                      Email me
+                    </a>
+                  </p>
+                </SheetDescription>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
         </div>
       </div>
     </>
